dbe8561192e0e433fbd8bca4529932a8d1d900c5
juaneco <juanceww82@gmail.com>
2023-12-24 03:43:21 -0700

    apple and orange solution

--- apple_and_orange/apple_and_orange.go
+++ apple_and_orange/apple_and_orange.go
@@ -8,37 +8,36 @@ import (
 
 func solution(lines []string, writer *bufio.Writer) {
 	// first line
-	var s = SplitIntsGetAt(lines[0], " ", 0)
-	var t = SplitIntsGetAt(lines[0], " ", 1)
+	s := SplitGetIntAt(lines[0], " ", 0)
+	t := SplitGetIntAt(lines[0], " ", 1)
 	// second line
-	var a = SplitIntsGetAt(lines[1], " ", 0)
-	var b = SplitIntsGetAt(lines[1], " ", 1)
+	a := SplitGetIntAt(lines[1], " ", 0)
+	b := SplitGetIntAt(lines[1], " ", 1)
 	// third line is ignored
 	// fourth line
-	var apples = SplitInts(lines[3], " ")
-	var oranges = SplitInts(lines[4], " ")
+	apples := SplitInts(lines[3], " ")
+	oranges := SplitInts(lines[4], " ")
 
-	var applesNumber int64 = 0
-	var orangesNumber int64 = 0
-
+	applesNumber := int64(0)
 	for _, da := range apples {
 		if a+da >= s && a+da <= t {
 			applesNumber++
 		}
 	}
 
+	orangesNumber := int64(0)
 	for _, db := range oranges {
 		if b+db <= t && b+db >= s {
 			orangesNumber++
 		}
 	}
 
-	Out(writer, fmt.Sprintf("%d\n%d", applesNumber, orangesNumber))
+	fmt.Fprintf(writer, "%d\n%d", applesNumber, orangesNumber)
 }
 
 func main() {
-	//RunWithFile(solution, "apple_and_orange/testcases/003.input")
-	RunWithString(solution, "7 11\n5 15\n3 2\n-2 2 1\n5 -6")
+	RunWithFile(solution, "apple_and_orange/testcases/001-in.txt")
+	//RunWithString(solution, "7 11\n5 15\n3 2\n-2 2 1\n5 -6")
 }
 
 // https://www.hackerrank.com/challenges/apple-and-orange/problem
