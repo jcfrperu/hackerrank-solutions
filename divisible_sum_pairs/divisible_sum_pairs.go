6881cc2df00500fa22b7db3e4de878cfe8856e23
juaneco <juanceww82@gmail.com>
2023-12-24 08:31:46 -0700

    divisible sum pairs solution

--- divisible_sum_pairs/divisible_sum_pairs.go
+++ divisible_sum_pairs/divisible_sum_pairs.go
@@ -6,8 +6,10 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
-	var result int32 = 0
+func solution(lines []string, writer *bufio.Writer) {
+	k := SplitGetIntAt(lines[0], " ", 1)
+	ar := SplitInts(lines[1], " ")
+	result := 0
 
 	for i := 0; i < len(ar)-1; i++ {
 		for j := i + 1; j < len(ar); j++ {
@@ -17,18 +19,13 @@ func divisibleSumPairs(n int32, k int32, ar []int32) int32 {
 		}
 	}
 
-	return result
-}
-
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
+	fmt.Fprintf(writer, "%d", result)
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	//RunWithFile(solution, "divisible_sum_pairs/testcases/001-in.txt")
+	RunWithString(solution, "6 3\n1 3 2 6 1 2 ")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
