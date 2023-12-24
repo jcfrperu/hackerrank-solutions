9ab12d588ee36b16ad9ffe52b4ea41bf79b3756a
juaneco <juanceww82@gmail.com>
2023-12-24 06:32:36 -0700

    staircase solution + updating go mod

--- go.mod
+++ go.mod
@@ -1,7 +1,5 @@
 module hackerrank-solutions
 
-go 1.20
+go 1.21.5
 
-require (
-	github.com/jcfrperu/go-competitive-programming v1.2.1
-)
+require github.com/jcfrperu/go-competitive-programming v1.2.1
--- staircase/staircase.go
+++ staircase/staircase.go
@@ -6,32 +6,27 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-// Complete the staircase function below.
-func staircase(n int32) {
+func solution(lines []string, writer *bufio.Writer) {
+	n := ParseInt(lines[0])
 
-	for i := int32(0); i < n; i++ {
-		for j := int32(0); j < n; j++ {
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
 			if j >= n-i-1 {
-				fmt.Printf("#")
+				fmt.Fprintf(writer, "#")
 			} else {
-				fmt.Printf(" ")
+				fmt.Fprintf(writer, " ")
 			}
 		}
 		if i != n-1 {
-			fmt.Printf("\n")
+			fmt.Fprintf(writer, "\n")
 		}
 	}
 }
 
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
-}
-
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	RunWithFile(solution, "staircase/testcases/001-in.txt")
+	//RunWithString(solution, "6")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/staircase/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
