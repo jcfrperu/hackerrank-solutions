ea2e0612f287d3aefc2ba4370435999bbe8e7e21
juaneco <juanceww82@gmail.com>
2023-12-24 10:19:52 -0700

    kangaroo solution

--- kangaroo/kangaroo.go
+++ kangaroo/kangaroo.go
@@ -6,35 +6,35 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-// Complete the kangaroo function below.
-func kangaroo(x1 float64, v1 float64, x2 float64, v2 float64) string {
+func solution(lines []string, writer *bufio.Writer) {
+	x1 := float64(SplitGetIntAt(lines[0], " ", 0))
+	v1 := float64(SplitGetIntAt(lines[0], " ", 1))
+	x2 := float64(SplitGetIntAt(lines[0], " ", 2))
+	v2 := float64(SplitGetIntAt(lines[0], " ", 3))
 
 	// problem's condition: v2 > v1
 	if v2 >= v1 {
-		return "NO"
+		fmt.Fprintf(writer, "NO")
+		return
 	}
 
 	// position must be integer
-	position := (x2 - x1) / (v1 - v2)
+	var position = (x2 - x1) / (v1 - v2)
 	positionInt := float64(int(position))
 
-	// if has decimal part
+	// if it has decimal part
 	if position-positionInt > 0 {
-		return "NO"
+		fmt.Fprintf(writer, "NO")
+		return
 	}
 
-	return "YES"
-}
-
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
+	fmt.Fprintf(writer, "YES")
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	//RunWithFile(solution, "kangaroo/testcases/001-in.txt")
+	RunWithString(solution, "0 2 5 3")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/kangaroo/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
