5f57704a947c42b47d026492bee9cb70afab8efa
juaneco <juanceww82@gmail.com>
2023-12-24 08:03:39 -0700

    compare the triplets solution

--- compare_the_triplets/compare_the_triplets.go
+++ compare_the_triplets/compare_the_triplets.go
@@ -6,12 +6,12 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-// Complete the compareTriplets function below.
-func compareTriplets(a []int32, b []int32) []int32 {
+func solution(lines []string, writer *bufio.Writer) {
+	a := SplitInts(lines[0], " ")
+	b := SplitInts(lines[1], " ")
 
 	result := []int32{0, 0}
-	for i, _ := range a {
-
+	for i := range a {
 		if a[i] > b[i] {
 			result[0] += 1
 		} else if b[i] > a[i] {
@@ -19,18 +19,13 @@ func compareTriplets(a []int32, b []int32) []int32 {
 		}
 	}
 
-	return result
-}
-
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
+	fmt.Fprintf(writer, "%d %d", result[0], result[1])
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	RunWithFile(solution, "compare_the_triplets/testcases/001-in.txt")
+	//RunWithString(solution, "5 6 7\n3 6 10")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/compare-the-triplets/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
