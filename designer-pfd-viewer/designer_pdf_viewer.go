52b7b2d5433cdb3496eb0019bc7a78f98ad1eca3
juaneco <juanceww82@gmail.com>
2023-12-24 08:11:24 -0700

    designer pdf viewer solution

--- designer-pfd-viewer/designer_pdf_viewer.go
+++ designer-pfd-viewer/designer_pdf_viewer.go
@@ -10,23 +10,23 @@ func solution(lines []string, writer *bufio.Writer) {
 	list := SplitInts(lines[0], " ")
 	values := lines[1]
 
-	var max = -1
+	maxValue := -1
 	for _, r := range values {
 		i := r - 'a'
-		if list[i] > max {
-			max = list[i]
+		if list[i] > maxValue {
+			maxValue = list[i]
 		}
 	}
 
-	result := max * len(values)
+	result := maxValue * len(values)
 
-	Out(writer, fmt.Sprintf("%d", result))
+	fmt.Fprintf(writer, "%d", result)
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	RunWithFile(solution, "designer-pfd-viewer/testcases/001-in.txt")
+	//RunWithString(solution, "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5\nabc")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
