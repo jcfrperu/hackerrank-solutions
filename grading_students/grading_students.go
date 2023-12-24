f6de5788cd82b783f5abe676ad304ba00b0c12ee
juaneco <juanceww82@gmail.com>
2023-12-24 11:02:41 -0700

    grading students solution

--- grading_students/grading_students.go
+++ grading_students/grading_students.go
@@ -6,38 +6,36 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-func gradingStudents(grades []int) []int {
+func solution(lines []string, writer *bufio.Writer) {
+	grades := make([]int, 0)
+	for _, line := range lines[1:] {
+		grades = append(grades, ParseInt(line))
+	}
 
-	var gradesRounded = make([]int, len(grades), len(grades))
-
+	var roundedGrades = make([]int, len(grades))
 	for i, grade := range grades {
-
-		gradesRounded[i] = grade
+		roundedGrades[i] = grade
 
 		if grade >= 38 && grade%5 != 0 {
-
 			multipleOfFive := grade + 1
 			for multipleOfFive%5 != 0 {
 				multipleOfFive++
 			}
 			if multipleOfFive-grade < 3 {
-				gradesRounded[i] = multipleOfFive
+				roundedGrades[i] = multipleOfFive
 			}
 		}
 	}
 
-	return gradesRounded
-}
-
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
+	for _, grade := range roundedGrades {
+		fmt.Fprintf(writer, "%d\n", grade)
+	}
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	//RunWithFile(solution, "grading_students/testcases/001-in.txt")
+	RunWithString(solution, "4\n73\n67\n38\n33")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/grading/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
