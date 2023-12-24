468aecb7d554c9cf81bf6889611b285b4d380a51
juaneco <juanceww82@gmail.com>
2023-12-24 08:08:41 -0700

    day of the programmer solution

--- day_of_the_programmer/day_of_the_programmer.go
+++ day_of_the_programmer/day_of_the_programmer.go
@@ -6,51 +6,46 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-// Complete the dayOfProgrammer function below.
-func dayOfProgrammer(year int) string {
+func solution(lines []string, writer *bufio.Writer) {
+	year := ParseInt(lines[0])
 
-	var isJulianCalendar = false
+	isJulianCalendar := false
 	if year <= 1917 {
 		isJulianCalendar = true
 	}
 
-	var isLeap = false
+	isLeap := false
 	if isJulianCalendar {
 		isLeap = year%4 == 0
 	} else {
 		isLeap = (year%400 == 0) || (year%4 == 0 && year%100 != 0)
 	}
 
-	var daysPerMonth = []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+	daysPerMonth := []int{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
 
 	if year == 1918 {
-		daysPerMonth[1] = 28 - 14 + 1 // caso especial en el anio 1918 que febrero tuvo menos dias porque empezo el nro 14
+		daysPerMonth[1] = 28 - 14 + 1 // special case, 1918, February had fewer days since it started the 14th
 	} else if isLeap {
 		daysPerMonth[1] = 29
 	}
 
-	var sum = 0
-	var month = 0
+	sum := 0
+	month := 0
 	for sum < 256 {
 		sum += daysPerMonth[month]
 		month++
 	}
 
-	// el dia 256 siempre sera en setiembre 9th mes
+	// 256 day is always be in september (9th month)
 	var day = 256 - (sum - daysPerMonth[month-1])
 
-	return fmt.Sprintf("%02d.%02d.%d", day, month, year)
-}
-
-func solution(lines []string, writer *bufio.Writer) {
-
-	Out(writer, fmt.Sprintf("%d", 4))
+	fmt.Fprintf(writer, "%02d.%02d.%d", day, month, year)
 }
 
 func main() {
-	//RunWithFile(solution, "PROBLEM/testcases/001.input")
-	RunWithString(solution, "1\n2 3")
+	//RunWithFile(solution, "day_of_the_programmer/testcases/001-in.txt")
+	RunWithString(solution, "2016")
 }
 
-// https://www.hackerrank.com/challenges/URL_PROBLEM/problem
+// https://www.hackerrank.com/challenges/day-of-the-programmer/problem
 // NOTE: read 'template-submit-code.go' file to submit your code to hackerrank.com
